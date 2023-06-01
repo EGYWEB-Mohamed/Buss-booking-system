afae3df1d0d47d12dad321ce876b4acd7ef98feb
Mohamed Said <me@msaied.com>
2023-06-01 15:24:22 +0300

    Install Filament & some dependencies

---
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use App\Filament\Resources\UserResource;
+use App\Models\User;
+use Filament\Resources\Pages\EditRecord;
+
+class EditUser extends EditRecord
+{
+    protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $getUser = User::where('email', $data['email'])->first();
+        if ($getUser) {
+            if (empty($data['password'])) {
+                $data['password'] = $getUser->password;
+            }
+        }
+
+        return $data;
+    }
+
+    protected function getTitle(): string
+    {
+        return trans('filament-user::user.resource.title.edit');
+    }
+}
